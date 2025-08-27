be213d3605530da57127b8f80fb0ba936b786caf
fernandoqm01 <fernandoquesadamora@gmail.com>
2025-08-27 19:55:03 +0000

    proyecto Listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[i] + adj[a] + noun[n] + ".com")
+      }
+
+    }
+
+  }
+
+
 };
